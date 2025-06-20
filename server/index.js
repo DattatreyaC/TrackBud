67bf2e114a1e52984ed3a1e03b310ae07740757f
unknown <dattatreya.chakraborty12@gmail.com>
2025-06-20 08:52:25 +0530

    test

--- server/index.js
+++ server/index.js
@@ -18,6 +18,11 @@ const allowedOrigins = [
     "https://trackbud.vercel.app/",
 ];
 
+app.use((req, res, next) => {
+    console.log("Incoming Origin:", req.headers.origin);
+    next();
+});
+
 app.use(
     cors({
         origin: function (origin, callback) {
