e2d8ab8f92ba722fef2151e8cc5b51f71de0a339
unknown <dattatreya.chakraborty12@gmail.com>
2025-08-25 23:33:13 +0530

    render url

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://trackbud-production.up.railway.app/api",
+    baseURL: "https://trackbud.onrender.com/api",
     headers: {
         "Content-Type": "application/json",
     },
