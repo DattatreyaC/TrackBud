49eed742ff2ecbe2c45e3463b64bade530fc5cdd
DattatreyaC <dattatreya.chakraborty12@gmail.com>
2026-02-28 11:57:18 +0530

    url and envv

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,12 +1,27 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
-    // baseURL: "https://trackbud.onrender.com/api",
-    baseURL: "http://localhost:5000/api",
-    headers: {
-        "Content-Type": "application/json",
-    },
-    withCredentials: true,
-});
+const env = import.meta.env.VITE_ENVIRONMENT;
+const url = import.meta.env.VITE_SERVER_URL;
+let axiosInstance = null;
+
+if (env === "prod") {
+    axiosInstance = axios.create({
+        baseURL: url,
+        // baseURL: "http://localhost:5000/api",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        withCredentials: true,
+    });
+} else {
+    axiosInstance = axios.create({
+        // baseURL: "https://trackbud.onrender.com/api",
+        baseURL: "http://localhost:5000/api",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        withCredentials: true,
+    });
+}
 
 export default axiosInstance;
--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+export const env = process.env.ENVIRONMENT;
 
 const allowedOrigins = [
     "http://localhost:5173",
